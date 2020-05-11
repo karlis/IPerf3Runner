0d35c0f990ecff5ea8ca0ef2af7465c7e08e72bb
Sergejs Smirnovs <sergejs.smirnovs@ui.com>
2020-05-11 13:37:13 +0300

    Fixed case sensitive file system issue

--- Package.swift
+++ Package.swift
@@ -3,19 +3,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "IPerf3Runner",
-    products: [
-        .library(name: "IPerf3Runner", targets: ["IPerf3Runner"]),
-    ],
-    dependencies: [
-         .package(url: "https://github.com/karlis/ciperf3", from: "1.0.3"),
-    ],
-    targets: [
-        .target(
-            name: "IPerf3Runner",
-            dependencies: ["ciperf3"]),
-        .testTarget(
-            name: "IPerf3RunnerTests",
-            dependencies: ["IPerf3Runner"]),
-    ]
+  name: "IPerf3Runner",
+  products: [
+    .library(name: "IPerf3Runner", targets: ["IPerf3Runner"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/karlis/ciperf3", from: "1.0.3"),
+  ],
+  targets: [
+    .target(name: "IPerf3Runner", dependencies: ["ciperf3"]),
+    .testTarget(name: "IPerf3RunnerTests", dependencies: ["IPerf3Runner"]),
+  ]
 )
